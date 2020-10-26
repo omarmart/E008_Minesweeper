d6a9f1f859deae65ebb804b4155d6221da78fe93
Omar <martinez.trujillo.omar@gmail.com>
2020-10-26 20:02:24 +0100

    clean main

--- src/main/java/com/omar/App.java
+++ src/main/java/com/omar/App.java
@@ -1,11 +1,12 @@
 package com.omar;
 
-/**
- * Hello world!
- *
- */
 public class App {
+
+    /***
+     * Launches the game
+     * @param args The arguments of the game. (Size and number of bombs)
+     */
     public static void main(String[] args) {
-        System.out.println("pinga World!");
+
     }
 }
