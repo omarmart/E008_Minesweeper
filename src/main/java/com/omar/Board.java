cedc6aada1d84ff998e523d06c634907ddd11d61
Omar <martinez.trujillo.omar@gmail.com>
2020-10-26 21:32:31 +0100

    added Board

---
+++ src/main/java/com/omar/Board.java
@@ -0,0 +1,49 @@
+package com.omar;
+
+public class Board {
+    private final int width;
+    private final int height;
+    private final Cell field[][];
+
+    public Board(int width, int height, int mines) {
+        this.width = width;
+        this.height = height;
+        this.field = new Cell[width][height];
+    }
+
+    /**
+     * Fills the board with the specified number of mines in random positions
+     * @param mines the number of mines that the board will contain
+     */
+    private static void fill(int mines) {
+        //TODO
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public void clickCell(int x, int y, Cell c) {
+        this.field[x][y].click();
+        draw();
+    }
+
+    public Cell getCell(int x, int y) {
+        return this.field[x][y];
+    }
+
+    /**
+     * Draws the board on the screen
+     */
+    public void draw() {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                System.out.println(this.field[i][j].getDisplay());
+            }
+        }
+    }
+}
