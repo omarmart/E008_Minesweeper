475d5690ccb0ceab63d35969e9ceacbbdb08b3f1
Omar <martinez.trujillo.omar@gmail.com>
2020-11-04 00:39:27 +0100

    added Display class

---
+++ src/main/java/com/omar/Display.java
@@ -0,0 +1,29 @@
+package com.omar;
+
+public class Display {
+
+    Board board;
+
+    /**
+     * Draws the board on the screen
+     */
+    public void draw() {
+        for (int i = 0; i < board.getWidth(); i++) {
+            for (int j = 0; j < board.getHeight(); j++) {
+                switch (board.getCell(i, j).getState()) {
+                    case FLAGGED:
+                        System.out.println("◘");
+                        break;
+                    case COVERED:
+                        System.out.println("■");
+                        break;
+                    case UNCOVERED:
+                        //TODO -- Board: CalculateSurrounding
+                        System.out.println("Result Surrounding");
+                        break;
+
+                }
+            }
+        }
+    }
+}
