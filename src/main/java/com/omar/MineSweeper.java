867d3964e6ff2afdca13fc447cf502da817428ab
Omar <martinez.trujillo.omar@gmail.com>
2020-11-06 21:53:18 +0100

    added size check for mines

--- src/main/java/com/omar/MineSweeper.java
+++ src/main/java/com/omar/MineSweeper.java
@@ -2,6 +2,7 @@ package com.omar;
 
 public class MineSweeper {
     private Board board;
+    private Coordinates display;
 
     /**
      * Creates a game with the given width, height, and mines
@@ -10,6 +11,13 @@ public class MineSweeper {
      * @param mines The number of mines
      */
     public MineSweeper(int width, int heigth, int mines) {
-        this.board = new Board(width, heigth, mines);
+        if (mines > (width * heigth)) {
+            System.out.println("There can't be more mines than cells on the board");
+            return;
+        }
+
+        //this.board = new Board(width, heigth, mines);
     }
+    //TODO:
+    //Meter aqui el check surrounding y pasarle los numeros al display
 }
