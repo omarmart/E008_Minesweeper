224afff4a6510aecf7a18efd7a5d2f1392b1610a
Omar <martinez.trujillo.omar@gmail.com>
2020-11-10 23:06:25 +0100

    add MineSweeper to main for testing

--- src/main/java/com/omar/App.java
+++ src/main/java/com/omar/App.java
@@ -7,6 +7,7 @@ public class App {
      * @param args The arguments of the game. (Size and number of bombs)
      */
     public static void main(String[] args) {
-
+        MineSweeper mineGame = new MineSweeper(10, 10, 50);
+        mineGame.play();
     }
 }
